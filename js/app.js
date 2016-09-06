319adc8fd1cd454fd3c4c923da8c5be544c2b73b
Michael Budde <MICHAEL.s.budde@gmail.com>
2016-09-06 11:02:10 -0700

    setting up routes

---
+++ js/app.js
@@ -0,0 +1,1 @@
+angular.module('fanStop', ['ui.router'])
---
+++ js/app.routes.js
@@ -0,0 +1,8 @@
+angular.module('fanStop')
+  .config(routes);
+
+routes.$inject = ['$urlRouterProvider', '$stateProvider'];
+
+function routes($urlRouterProvider, $stateProvider) {
+  
+}
